6a1b7a6bd8ba73b1a42cafc6fcc9074377f341fc
kodeeramchandran <koteeswaran.ramachandran@sixt.com>
2022-06-04 18:49:32 +0200

    fixed for mozila

--- sw.js
+++ sw.js
@@ -11,3 +11,18 @@ self.addEventListener('push', function(event) {
   
     event.waitUntil(self.registration.showNotification(title, options));
   });
+
+  // Add an event listener to handle notification clicks
+self.addEventListener('notificationclick', function (event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  
+    const title = 'Push web by kodee';
+    const options = {
+      body: event.data.text(),
+      icon: 'images/icon.png',
+      badge: 'images/badge.png'
+    };
+  
+    event.waitUntil(self.registration.showNotification(title, options));
+});
